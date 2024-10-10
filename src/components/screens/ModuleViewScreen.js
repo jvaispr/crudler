0cf402849171f6d29551d9e523c8789b98095da5
jvaispr <jpinderromero@gmail.com>
2024-10-10 17:20:42 +0100

    Completed second video changes (#1)

---
+++ src/components/screens/ModuleViewScreen.js
@@ -0,0 +1,22 @@
+
+import { StyleSheet, Text } from 'react-native';
+import Screen from '../layout/Screen';
+
+const ModuleViewScreen = () => {
+// Initializations ---------------------
+// State -------------------------------
+// Handlers ----------------------------
+// View --------------------------------
+
+  return (
+    <Screen>
+
+      <Text>View</Text>
+
+    </Screen>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+export default ModuleViewScreen;
