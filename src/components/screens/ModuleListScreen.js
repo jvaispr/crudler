77d2c0a4b2924122dc88520ccfd5c26145af2ba2
jvaispr <jpinderromero@gmail.com>
2024-10-16 18:02:19 +0100

    finished populating moduleListView Screen (#2)

--- src/components/screens/ModuleListScreen.js
+++ src/components/screens/ModuleListScreen.js
@@ -1,22 +1,50 @@
 
-import { StyleSheet, Text } from 'react-native';
+import { Pressable, StyleSheet, ScrollView, Text, View } from 'react-native';
 import Screen from '../layout/Screen';
+
+import initialModules from '../../data/modules.js';
 
 const ModuleListScreen = () => {
 // Initializations ---------------------
+const modules = initialModules;
 // State -------------------------------
 // Handlers ----------------------------
-// View --------------------------------
+const handleSelect = () => alert('Item Selected');
 
+// View --------------------------------
   return (
     <Screen>
+      <ScrollView style = {styles.container}>
+      {modules.map((module) => {
+        return (
+          
+          <Pressable key={module.ModuleCode} onPress={handleSelect}>
+          <View style={styles.item}>
+            <Text style={styles.text}>
+              {module.ModuleCode} {module.ModuleName}
+            </Text>
+          </View>
+          </Pressable>
 
-      <Text>List</Text>
-
+        );
+      })}
+      </ScrollView>
     </Screen>
+    
   );
-}
+};
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+container:{},
+item:{
+  paddingVertical: 15,
+  borderTopWidth: 1,
+  borderColor: "lightgrey",
+},
+text:{
+  ontsize: 16,
+},
+
+});
 
 export default ModuleListScreen;
