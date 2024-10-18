40ba743c00eba5b499a901f548d83029bd69beed
jvaispr <jpinderromero@gmail.com>
2024-10-18 02:45:27 +0100

    fixes

--- src/components/UI/Button.js
+++ src/components/UI/Button.js
@@ -1,36 +1,21 @@
 import {Pressable, StyleSheet, Text, View} from 'react-native';
 
-<<<<<<< HEAD
-const Button = ({label, onClick}) => {
-=======
 export const Button = ({label, icon, onClick, styleLabel, styleButton}) => {
 
->>>>>>> ecd33f1b650b7fbad63a0e3a7da2ae1e55d9cf44
 
 // Initializations ---------------------
 // State -------------------------------
 // Handlers ----------------------------
 // View --------------------------------
 return (
-<<<<<<< HEAD
- <Pressable onPress={onClick} style={styles.button}>
-    <Text style={styles.label}>{label}</Text>
- </Pressable>
-=======
-
  <Pressable onPress={onClick} style={[styles.Button, styleButton]}>
     {icon ? icon : null}
     <Text style={[styles.label, styleLabel]}>{label}</Text>
-
->>>>>>> ecd33f1b650b7fbad63a0e3a7da2ae1e55d9cf44
+    </Pressable>
 
 );
 };
 
-<<<<<<< HEAD
-const styles = StyleSheet.create({
-button: {
-=======
 export const ButtonTray = ({children}) => {
 
     // Initializations ---------------------
@@ -51,7 +36,6 @@ ButtonTray: {
 
 Button: {
     minHeight: 50,
->>>>>>> ecd33f1b650b7fbad63a0e3a7da2ae1e55d9cf44
     borderWidth: 1,
     borderRadius: 7,
     borderColor: 'grey',
@@ -61,13 +45,10 @@ Button: {
     padding: 3,
     flex: 1,
 
-<<<<<<< HEAD
-=======
     flexDirection: 'row',
     gap: 5,
 
 
->>>>>>> ecd33f1b650b7fbad63a0e3a7da2ae1e55d9cf44
 },
 label: {
     fontSize: 16,
@@ -80,14 +61,11 @@ label: {
 
 
 
-
-<<<<<<< HEAD
-});
-=======
-
 );
 
->>>>>>> ecd33f1b650b7fbad63a0e3a7da2ae1e55d9cf44
+
+
+
 
 export default Button;
 
--- src/components/UI/Icons.js
+++ src/components/UI/Icons.js
@@ -1,26 +1,5 @@
 import { MaterialIcons } from '@expo/vector-icons';
 
-<<<<<<< HEAD
-
-const Icons = {};
-const Add = () => <MaterialIcons name='add' size={16} />;
-const Close = () => <MaterialIcons name='close' size={16} />;
-const Delete = () => <MaterialIcons name='delete' size={16} />;
-const Edit = () => <MaterialIcons name='edit' size={16} />;
-const Submit = () => <MaterialIcons name='check' size={16} />;
-
-
-// Compose
-Icons.Add = Add;
-Icons.Close = Close;
-Icons.Delete = Delete;
-Icons.Edit = Edit;
-Icons.Submit = Submit;
-export default Icons;
-
-
-
-=======
 const Icons = {};
 
 const Add = () => <MaterialIcons name="add" size={16}/>
@@ -34,4 +13,3 @@ Icons.Delete = Delete;
 
 export default Icons;
 
->>>>>>> ecd33f1b650b7fbad63a0e3a7da2ae1e55d9cf44
